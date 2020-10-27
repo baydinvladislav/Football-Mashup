2adbb73dde689c46c6950bb001fd2fc65cfcb89b
Baydin <baydinvladislav@yandex.ru>
2020-10-27 13:25:39 +0300

    refactored code using promises

--- football.js
+++ football.js
@@ -1,27 +1,31 @@
 const key = 'ff1cf7c09emsh9d03a146d7f500ep1eccddjsn49ed21d9d25b';
 const source = 'api-football-v1.p.rapidapi.com';
-const urlAPI = 'https://api-football-v1.p.rapidapi.com/v2/topscorers/64';
+const urlAPI = 'https://api-football-v1.p.rapidapi.com/v2/topscorers/754';
 const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 
 const strikersList = document.querySelector('#strikers')
 const btn = document.querySelector('button');
 
-// Make an AJAX request
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      console.log(data);
-      callback(data);
+// Make an AJAX requests
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        resolve(data);
+      } else {
+        reject( Error(xhr.statusText) );
+      }
+    };
+    if (url == urlAPI) { // API football requires setted request header
+    xhr.setRequestHeader('x-rapidapi-host', source);
+    xhr.setRequestHeader('x-rapidapi-key', key);
     }
-  };
-  if (url == urlAPI) { // API football requires setted request header
-  xhr.setRequestHeader('x-rapidapi-host', source);
-  xhr.setRequestHeader('x-rapidapi-key', key);
-  }
-  xhr.send();
+    xhr.onerror = () => reject( Error('A network error occured') );
+    xhr.send();
+  });
 }
 
 // Generate HTML
@@ -38,8 +42,19 @@ function generateHTML(data) {
 
 // Receive information from Wiki
 function getProfiles(json) {
-    getJSON(wikiUrl + json.api.topscorers[0].firstname + ' ' + json.api.topscorers[0].lastname, generateHTML);
+    return getJSON(wikiUrl + json.api.topscorers[0].firstname + ' ' + json.api.topscorers[0].lastname);
 }
 
 
-getJSON(urlAPI, getProfiles)
+btn.addEventListener('click', (event) => {
+  event.target.textContent = 'Loading...'
+
+  getJSON(urlAPI)
+    .then(getProfiles)
+    .then(generateHTML)
+    .catch( err => {
+      strikersList.innerHTML = '<h3>Something went wrong!</h3>'
+      console.log(err)
+    })
+    .finally( () =>  event.target.remove() )
+});
